a40666b136ee6cfbfcaf16ff959f81e788c6a406
w_pengliufeng(彭刘锋) <1565600940@qq.com>
2021-07-28 15:24:56 +0800

    后端添加token校验

--- yun-music-total/yun-music-server/src/main/java/com/plf/yunmusicserver/config/mvc/FilterMvcConfig.java
+++ yun-music-total/yun-music-server/src/main/java/com/plf/yunmusicserver/config/mvc/FilterMvcConfig.java
@@ -3,14 +3,13 @@ package com.plf.yunmusicserver.config.mvc;
 import com.plf.yunmusicserver.interceptor.AuthenticationInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class FilterMvcConfig  extends WebMvcConfigurationSupport {
+public class FilterMvcConfig implements WebMvcConfigurer {
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AuthenticationInterceptor()).addPathPatterns("/api/**");
-        super.addInterceptors(registry);
     }
 }
